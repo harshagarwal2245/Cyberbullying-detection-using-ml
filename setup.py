9cdb2a28c9678204e23048b98321da31733d7e44
Harsh Agarwal <agarwalharsh244@gmail.com>
2021-10-05 17:41:14 -0700

    Created setup.py package and runned pip install -e.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="It a Cybertrolling detection package",
+    author="Harsh Agarwal Jagruti Jadhav Babita Jaybhaye Komal Nagar",
+    packages=find_packages(),
+    license="MIT"
+)
