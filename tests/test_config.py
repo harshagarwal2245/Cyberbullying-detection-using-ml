70d1dacc5c82752f8868ca630a6c3bbe89817413
Harsh Agarwal <agarwalharsh244@gmail.com>
2021-10-05 17:13:04 -0700

    Created sample test case with pytest

---
+++ tests/test_config.py
@@ -0,0 +1,10 @@
+"""
+to get function identified by pytest always write function name starting with 
+test example test_env
+function should always contain assert function if assert is true test is passed
+"""
+
+def test_generic():
+    a=2
+    b=2
+    assert a == b
